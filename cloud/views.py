c00589406ad4aa1ff43ee6e9390251dbc2b461bb
Tuan <doanhtuan14111997@gmail.com>
2020-05-10 16:15:27 +0700

    first commit

--- cloud/views.py
+++ cloud/views.py
@@ -3232,9 +3232,26 @@ def signin(request):
     if request.method == 'POST':
         xuser = request.POST.get('txtuser')
         xpass = request.POST.get('txtpass')
-        error = str(xuser) + str(xpass)
+        data = models.ZUser.objects.filter(Q(username=xuser), Q(password=xpass), Q(active=1))
+        if data.count():
+            request.session['id'] = data[0].id
+            request.session['name'] = data[0].name
+            request.session['kind'] = data[0].kind
+            request.session['phone'] = data[0].phone
+            request.session['address'] = data[0].adress
+            request.session['email'] = data[0].email
+            request.session['other_info'] = data[0].other_info
+            request.session.set_expiry(0)
+            if request.session['kind'] == 'citizen':
+                return redirect('citizenHome')
+            elif request.session['kind'] == 'factory':
+                facilityID = models.Sites.objects.filter(userID_id=request.session['id'])[0].siteid
+                return redirect('facilitiesDisplay', facilityID)
+            else:
+                return redirect('manager', 3)
+        else:
+            error = "Tài khoản hoặc mật khẩu không đúng"
     return render(request, 'Home/index.html', {'error': error})
-
 def logout(request):
     try:
         del request.session['id']
